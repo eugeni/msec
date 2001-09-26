1572fc5e109642e985b6992a8148c390100f6cf8
florin <florin@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-09-26 14:24:06 +0000

    remove the -E option in the AddRules grep
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/msec/trunk@149642 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- init-sh/lib.sh
+++ init-sh/lib.sh
@@ -33,7 +33,7 @@ AddRules() {
 		echo "Modifying config in ${file}..."
 	fi	
 	
-	if ! grep -Eqx "^${string}" ${file}; then
+	if ! grep -qx "^${string}" ${file}; then
 		echo -e "${COMMENT}" >> ${file};
 		echo -e "${string}" >> ${file};
 	fi

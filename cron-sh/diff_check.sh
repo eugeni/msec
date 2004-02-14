b525f1e32261f0093667d5984dae04e0abd8eb68
flepied <flepied@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2004-02-14 19:24:55 +0000

    allow % in file names [bug #6144] (Sven Hoexter)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/msec/trunk@150124 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- cron-sh/diff_check.sh
+++ cron-sh/diff_check.sh
@@ -27,10 +27,10 @@ if [[ ${CHECK_SUID_ROOT} == yes ]]; then
     if [[ -f ${SUID_ROOT_YESTERDAY} ]]; then
 	if ! diff -u ${SUID_ROOT_YESTERDAY} ${SUID_ROOT_TODAY} > ${SUID_ROOT_DIFF}; then
 	    printf "\nSecurity Warning: Change in Suid Root files found :\n" >> ${TMP}
-	    grep '^+' ${SUID_ROOT_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${SUID_ROOT_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-       Newly added suid root file : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${SUID_ROOT_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${SUID_ROOT_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer present suid root file : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -44,10 +44,10 @@ if [[ ${CHECK_SGID} == yes ]]; then
     if [[ -f ${SGID_YESTERDAY} ]]; then
 	if ! diff -u ${SGID_YESTERDAY} ${SGID_TODAY} > ${SGID_DIFF}; then
             printf "\nSecurity Warning: Changes in Sgid files found :\n" >> ${TMP}
-	    grep '^+' ${SGID_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${SGID_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-       Newly added sgid file : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${SGID_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${SGID_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer present sgid file : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -62,10 +62,10 @@ if [[ ${CHECK_WRITABLE} == yes ]]; then
 	diff -u ${WRITABLE_YESTERDAY} ${WRITABLE_TODAY} > ${WRITABLE_DIFF}
 	if [ -s ${WRITABLE_DIFF} ]; then
 	    printf "\nSecurity Warning: Change in World Writable Files found :\n" >> ${TMP}
-	    grep '^+' ${WRITABLE_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${WRITABLE_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-       Newly added writable file : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${WRITABLE_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${WRITABLE_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer present writable file : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -80,10 +80,10 @@ if [[ ${CHECK_UNOWNED} == yes ]]; then
 	diff -u ${UNOWNED_USER_YESTERDAY} ${UNOWNED_USER_TODAY} > ${UNOWNED_USER_DIFF}
 	if [ -s ${UNOWNED_USER_DIFF} ]; then
 	    printf "\nSecurity Warning: the following files aren't owned by an user :\n" >> ${TMP}
-	    grep '^+' ${UNOWNED_USER_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${UNOWNED_USER_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-       Newly added un-owned file : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${UNOWNED_USER_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${UNOWNED_USER_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer present un-owned file : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -93,10 +93,10 @@ if [[ ${CHECK_UNOWNED} == yes ]]; then
 	diff -u ${UNOWNED_GROUP_YESTERDAY} ${UNOWNED_GROUP_TODAY} > ${UNOWNED_GROUP_DIFF}
 	if [ -s ${UNOWNED_GROUP_DIFF} ]; then
 	    printf "\nSecurity Warning: the following files aren't owned by a group :\n" >> ${TMP}
-	    grep '^+' ${UNOWNED_GROUP_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${UNOWNED_GROUP_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-       Newly added un-owned file : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${UNOWNED_GROUP_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${UNOWNED_GROUP_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer present un-owned file : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -111,7 +111,7 @@ if [[ ${CHECK_SUID_MD5} == yes  ]]; then
     if [[ -f ${SUID_MD5_YESTERDAY} ]]; then
 	diff -u ${SUID_MD5_YESTERDAY} ${SUID_MD5_TODAY} > ${SUID_MD5_DIFF}
 	if [ -s ${SUID_MD5_DIFF} ]; then
-	    grep '^+' ${SUID_MD5_DIFF} | grep -vw "^+++ " | sed 's|^.||' | awk '{print $2}' | while read file; do
+	    grep '^+' ${SUID_MD5_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | awk '{print $2}' | while read file; do
 		if cat ${SUID_MD5_YESTERDAY} | awk '{print $2}' | grep -qw ${file}; then
 		    if [[ ${ctrl_md5} == 0 ]]; then
 			printf "\nSecurity Warning: the md5 checksum for one of your SUID files has changed,\n" >> ${TMP}
@@ -133,10 +133,10 @@ if [[ ${CHECK_OPEN_PORT} == yes ]]; then
 	diff -u ${OPEN_PORT_YESTERDAY} ${OPEN_PORT_TODAY} 1> ${OPEN_PORT_DIFF}
 	if [ -s ${OPEN_PORT_DIFF} ]; then
 	    printf "\nSecurity Warning: There are modifications for port listening on your machine :\n" >> ${TMP}
-	    grep '^+' ${OPEN_PORT_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${OPEN_PORT_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-  Opened ports : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${OPEN_PORT_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${OPEN_PORT_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- Closed ports  : ${file}\n"
 	    done >> ${TMP}
         fi
@@ -150,10 +150,10 @@ if [[ ${RPM_CHECK} == yes ]]; then
 	diff -u ${RPM_QA_YESTERDAY} ${RPM_QA_TODAY} > ${RPM_QA_DIFF}
 	if [ -s ${RPM_QA_DIFF} ]; then
 	    printf "\nSecurity Warning: These packages have changed on the system :\n" >> ${TMP}
-	    grep '^+' ${RPM_QA_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${RPM_QA_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-   Newly installed package : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${RPM_QA_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${RPM_QA_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer present package : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -162,10 +162,10 @@ if [[ ${RPM_CHECK} == yes ]]; then
 	diff -u ${RPM_VA_YESTERDAY} ${RPM_VA_TODAY} > ${RPM_VA_DIFF}
 	if [ -s ${RPM_VA_DIFF} ]; then
 	    printf "\nSecurity Warning: These files belonging to packages have changed of status on the system :\n" >> ${TMP}
-	    grep '^+' ${RPM_VA_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${RPM_VA_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-     Newly modified : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${RPM_VA_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${RPM_VA_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer modified : ${file}\n"
 	    done >> ${TMP}
 	fi
@@ -174,10 +174,10 @@ if [[ ${RPM_CHECK} == yes ]]; then
 	diff -u ${RPM_VA_CONFIG_YESTERDAY} ${RPM_VA_CONFIG_TODAY} > ${RPM_VA_CONFIG_DIFF}
 	if [ -s ${RPM_VA_CONFIG_DIFF} ]; then
 	    printf "\nSecurity Warning: These config files belonging to packages have changed of status on the system :\n" >> ${TMP}
-	    grep '^+' ${RPM_VA_CONFIG_DIFF} | grep -vw "^+++ " | sed 's|^.||' | while read file; do
+	    grep '^+' ${RPM_VA_CONFIG_DIFF} | grep -vw "^+++ " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t-     Newly modified : ${file}\n"
 	    done >> ${TMP}
-	    grep '^-' ${RPM_VA_CONFIG_DIFF} | grep -vw "^--- " | sed 's|^.||' | while read file; do
+	    grep '^-' ${RPM_VA_CONFIG_DIFF} | grep -vw "^--- " | sed 's|^.||'|sed -e 's/%/%%/g' | while read file; do
 		printf "\t\t- No longer modified : ${file}\n"
 	    done >> ${TMP}
 	fi

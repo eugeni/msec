da32cf2b7a2f89de728965ae92c789a5bba02824
eugeni <eugeni@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-01-07 22:05:24 +0000

    Typo fixed.
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/msec/trunk@250958 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- src/msec/libmsec.py
+++ src/msec/libmsec.py
@@ -755,7 +755,7 @@ class MSEC:
         pass
 
     def create_server_link(self, param):
-        '''  Creates the symlink /etc/security/msec/server to point to /etc/security/msec/server.<SERVER_LEVEL>. The /etc/security/msec/server is used by chkconfig --add to decide to add a service if it is present in the file during the installation of packages.'''
+        '''  Creates the symlink /etc/security/msec/server to point to /etc/security/msec/server.SERVER_LEVEL. The /etc/security/msec/server is used by chkconfig --add to decide to add a service if it is present in the file during the installation of packages.'''
         __params__ = ["no", "default", "secure"]
 
         server = self.configfiles.get_config_file(SERVER)
